54c9a1b7f1c4d55a0d594d91f557a3901fa94644
satish <satish@test.com>
2025-04-01 14:17:53 -0400

    updated masking to handle not just strings but other data types

--- src/main/java/com/usbank/fraud/MaskingSerializer.java
+++ src/main/java/com/usbank/fraud/MaskingSerializer.java
@@ -5,22 +5,27 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.*;
 
-public class MaskingSerializer extends JsonSerializer<String> {
+public class RecursiveMaskingSerializer extends JsonSerializer<Object> {
+
     private final Mask mask;
 
-    public MaskingSerializer(Mask mask) {
+    public RecursiveMaskingSerializer(Mask mask) {
         this.mask = mask;
     }
 
     @Override
+    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        serializeValue(value, gen, serializers);
+    }
+
     private void serializeValue(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
         if (value == null) {
             gen.writeNull();
         } else if (value instanceof String str) {
             gen.writeString(applyMask(str));
-        } else if (value instanceof Number num) {
-            gen.writeString(applyMask(num.toString())); // ← Mask numbers as strings
         } else if (value instanceof Collection<?> collection) {
             gen.writeStartArray();
             for (Object item : collection) {
@@ -31,7 +36,8 @@ public class MaskingSerializer extends JsonSerializer<String> {
             gen.writeStartArray();
             int length = Array.getLength(value);
             for (int i = 0; i < length; i++) {
-                serializeValue(Array.get(value, i), gen, serializers);
+                Object item = Array.get(value, i);
+                serializeValue(item, gen, serializers);
             }
             gen.writeEndArray();
         } else if (value instanceof Map<?, ?> map) {
@@ -42,7 +48,23 @@ public class MaskingSerializer extends JsonSerializer<String> {
             }
             gen.writeEndObject();
         } else {
-            gen.writeObject(value); // fallback for unknown types
+            // Fallback to normal Jackson serialization
+            gen.writeObject(value);
         }
+    }
+
+    private String applyMask(String input) {
+        int showFirst = mask.showFirst();
+        int showLast = mask.showLast();
+        char maskChar = mask.maskChar();
+
+        int len = input.length();
+        if (showFirst + showLast >= len) return input;
+
+        StringBuilder masked = new StringBuilder();
+        masked.append(input, 0, showFirst);
+        masked.append(String.valueOf(maskChar).repeat(len - showFirst - showLast));
+        masked.append(input.substring(len - showLast));
+        return masked.toString();
     }
 }
